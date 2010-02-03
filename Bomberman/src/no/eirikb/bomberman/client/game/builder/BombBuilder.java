90114b2e84cac1d687b0b5f04dff57951e4de34e
Eirik Brandtzæg <eirikb@eirikb.no>
2010-02-03 19:08:52 +0100

    People can't decide what they want about bombdropping... Fixed bug for placing bombs outside game panel

--- Bomberman/src/no/eirikb/bomberman/client/game/builder/BombBuilder.java
+++ Bomberman/src/no/eirikb/bomberman/client/game/builder/BombBuilder.java
@@ -28,30 +28,34 @@ public class BombBuilder {
                 int spriteX = (int) ((player.getX() + (settings.getImgSize() / 2)) / settings.getImgSize());
                 int spriteY = (int) ((player.getY() + (settings.getImgSize() / 2)) / settings.getImgSize());
 
-                switch (player.getLastWay()) {
-                    case LEFT:
-                        spriteX--;
-                        break;
-                    case UP:
-                        spriteY--;
-                        break;
-                    case RIGHT:
-                        spriteX++;
-                        break;
-                    case DOWN:
-                        spriteY++;
-                        break;
-                }
+                // Olav want to drop the bombs in front, Benjamin wants to just drop em down.
+                // I say fuck this shit...
+//                switch (player.getLastWay()) {
+//                    case LEFT:
+//                        spriteX--;
+//                        break;
+//                    case UP:
+//                        spriteY--;
+//                        break;
+//                    case RIGHT:
+//                        spriteX++;
+//                        break;
+//                    case DOWN:
+//                        spriteY++;
+//                        break;
+//                }
 
-                if (sprites[spriteX][spriteY] != null) {
-                    spriteX = (int) ((player.getX() + (settings.getImgSize() / 2)) / settings.getImgSize());
-                    spriteY = (int) ((player.getY() + (settings.getImgSize() / 2)) / settings.getImgSize());
-                }
-                if (sprites[spriteX][spriteY] == null) {
-                    Bomb bomb = new Bomb(spriteX, spriteY, player, settings.getBombTimer(), player.getBombPower());
-                    if (bomb != null && bomb.getSpriteX() >= 0 && bomb.getSpriteY() >= 0
-                            && bomb.getSpriteX() < sprites.length && bomb.getSpriteY() < sprites[0].length) {
-                        return bomb;
+                if (spriteX >= 0 && spriteX < sprites.length && spriteY >= 0 && spriteY < sprites[0].length) {
+                    if (sprites[spriteX][spriteY] != null) {
+                        spriteX = (int) ((player.getX() + (settings.getImgSize() / 2)) / settings.getImgSize());
+                        spriteY = (int) ((player.getY() + (settings.getImgSize() / 2)) / settings.getImgSize());
+                    }
+                    if (sprites[spriteX][spriteY] == null) {
+                        Bomb bomb = new Bomb(spriteX, spriteY, player, settings.getBombTimer(), player.getBombPower());
+                        if (bomb != null && bomb.getSpriteX() >= 0 && bomb.getSpriteY() >= 0
+                                && bomb.getSpriteX() < sprites.length && bomb.getSpriteY() < sprites[0].length) {
+                            return bomb;
+                        }
                     }
                 }
             }
