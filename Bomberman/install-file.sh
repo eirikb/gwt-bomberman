0824ed9312169797c88e571024cdc6344bb85e7c
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-10 00:33:39 +0200

    Dependencies in maven - for now

---
+++ Bomberman/install-file.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=src/main/webapp/WEB-INF/lib/gwteventservice-1.1.1.jar -DgroupId=gwt -DartifactId=gwt-event-service -Dversion=1.1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=src/main/webapp/WEB-INF/lib/gwt-image-loader-1.1.1.jar -DgroupId=gwt -DartifactId=gwt-image-loader -Dversion=1.1.1 -Dpackaging=jar
